78d3794d49474548e58180a32517d2fd0e173cf0
sanjeevi01 <sangv.r99@gmail.com>
2019-06-15 13:50:11 +0530

    initial Commit

---
+++ scilab_theme/js/nav.js
@@ -0,0 +1,5 @@
+// $( '#scilab_theme_nav .navbar-nav a' ).on( 'click', function () {
+// 	$( '#scilab_theme_nav .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+// 	$( this ).parent( 'li' ).addClass( 'active' );
+// // });
+
